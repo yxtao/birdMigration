6b18f27afd2d29a2a0bdd820c77ab8e53117c5fd
yinxiatao <yinxiatao@gmail.com>
2022-04-06 17:23:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Cat from './Cat';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Cat x={20} y={500} speed={50} />
+      <Cat x={60} y={100} speed={150} />
     </div>
+     
   );
 }
 
