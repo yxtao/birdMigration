0445db30b153204662574f1b215fc65f7599ada1
yinxiatao <yinxiatao@gmail.com>
2022-04-12 09:42:03 -0700

    Add survial rate

--- src/Board.js
+++ src/Board.js
@@ -6,69 +6,90 @@ function Board() {
   const [results, setResults] = useState([]);
   const [winners, setWinners] = useState([]);
   const [animals, setAnimals] = useState([]);
-  const populationNumValue = useRef();
-  const [timer, setTimer] = useState(0);
-  const [stop,setStop] = useState("no")
-  
+  const [populationNumValue, setPopulationNumValue] = useState(0);
+  const [populationNum, setPopulationNum] = useState(0)
+  const [survivalRate, setSurvivalRate] = useState(0)
+  const [targetSurvialRateValue, setTargetSurvialRateValue] = useState(1)
+  const [targetSurvialRate, setTargetSurvialRate] = useState(1);
+  const [nextGen, setNextGen] = useState(false)
+  const [parents, setParents] = useState([]);
+  const [generationCounts, setGenerationCounts] = useState(0);
+  const [flag,setFlag] = useState("none")
+
   const handleData = (e)=>{
-     setResults((pre)=>[...pre,e])
+     setResults((pre)=>[...pre, e])
      if(e.winner === true){
-       setWinners((pre)=>[...pre,e])
+       setWinners((pre)=>[...pre, e]);
+       setParents((pre)=>[...pre, e])
      }
   }
-
+ 
  function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
+  const handleTargetSurvialRateValueChange = (e)=>{
+    setTargetSurvialRateValue(e.target.value);
+  }
+  const handlePopulationNumValueChange = (e)=>{
+    setPopulationNumValue(Number(e.target.value))
+  }
  const confirmPopulationNum = ()=>{
+   populationNumValue>0 && setPopulationNum(populationNumValue)
+   setTargetSurvialRate(targetSurvialRateValue*0.01);
    setResults([]);
    setWinners([]);
    setAnimals([]);
-   setTimer(0)
  }
-
   const initializePopulation = ()=>{ 
+    setGenerationCounts(1)
+    setResults([]);
+    setWinners([]);
+    setAnimals([]);
+    setParents([]);
     let temp = [];
-    for(let i=0; i<populationNumValue.current.value;i++){
+    for(let i=0; i<populationNum;i++){
         let creature = {};
         creature.x = getRandomArbitrary(10,100);
-        creature.speedX = getRandomArbitrary(5,20);
+        creature.speedX = getRandomArbitrary(20,30);
         creature.waveY = getRandomArbitrary(1,10);
         temp.push(creature)
     }
     setAnimals(temp);
-    let count = 0;
-    const intervalId = setInterval(()=>{ 
-      count++;
-      if(count > 5){
-         clearInterval(intervalId);
-         setStop("yes")
-         return;
-      }else{
-        setTimer(pre=> pre+1);
-      }
-  }, 1000)
   }
-  
- 
+  useEffect(()=>{ 
+    if(populationNum>=30 && survivalRate<targetSurvialRate && results.length === populationNum ){
+    setTimeout(()=>{
+      setResults([]);
+      setWinners([]);
+      setAnimals([]);
+      setNextGen(true)
+    }, 500)
+    }
+  }, [survivalRate, results, populationNum, targetSurvialRate])
+
   useEffect(()=>{
-      if(winners.length === 0 && stop === "yes") {
-      let temp = [];
-      for(let i=0; i<10;i++){
-          let creature = {};
-          creature.x = getRandomArbitrary(10,100);
-          creature.speedX = getRandomArbitrary(5,20);
-          creature.waveY = getRandomArbitrary(1,10);
-          temp.push(creature)
-      }
-      setAnimals(temp);
-     }
- },[ winners, stop])
+    let temp = [...parents];
+    setFlag(parents.length)
+    if(nextGen === true && animals.length===0){
+      setGenerationCounts(pre=>pre+1)
+      for(let i=0; i<populationNum-parents.length;i++){
+        let creature = {};
+        creature.x = getRandomArbitrary(10,100);
+        creature.speedX = getRandomArbitrary(20,30);
+        creature.waveY = getRandomArbitrary(1,10);
+        temp.push(creature)
+    }
+    setTimeout(()=>{setAnimals(temp)},1000)
+    setParents([])
+    setNextGen(false)
+    }
+  },[nextGen, populationNum, animals, parents])
+
+  useEffect(()=>{
+     setSurvivalRate(winners.length/populationNum);
+  },[results, populationNum, winners])
 
 
-useEffect(()=>{
-
-}, [results])
   return (
     <div style={{display : "flex"}}>
       <div style={{height:"700px", width:"800px", border:"1px solid red", marginLeft:"30px", marginTop:"50px",background:"beige"}}>
@@ -82,24 +103,41 @@ useEffect(()=>{
       </div>
       <div style={{width:"500px", border:"1px solid blue", marginTop:"50px"}}>
         <div>
+          <p>{flag}</p>
+          population number
           <input 
             type="number"
-            min="1"
+            min="30"
             max="100"
-            ref = {populationNumValue}  />
+            value={populationNumValue}
+            onChange={(e)=>handlePopulationNumValueChange(e)} />
+        </div>
+        <div>
+          survival rate
+          <input 
+            type="number"
+            min="20"
+            max="100"
+            value={targetSurvialRateValue}
+            onChange={(e)=>handleTargetSurvialRateValueChange(e)} /> %
         </div>
           <button 
               className= "btn btn-outline-primary m-1"
               onClick={confirmPopulationNum}>confirm</button>
         <div>
-        {timer} {stop}
           <button 
               className="btn btn-outline-success mt-5"
               onClick={initializePopulation}> 
               initialize population </button>
-        </div>
-         
-          winners: {winners.map((winner,index)=><p key={index}>{winner.index} </p>)}
+        </div> 
+          {/* winners: {winners.map((winner,index)=><p key={index}>{winner.index} </p>)} */}
+          <div>
+            generation counts {generationCounts} Target {targetSurvialRate}
+          </div>
+          <div>
+            survival rate : {survivalRate} {nextGen} 
+            <p>{results.length} {populationNum}</p>
+          </div>
       </div>
      </div>
   );
